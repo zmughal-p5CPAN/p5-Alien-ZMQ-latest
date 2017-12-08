1a86da631cd4288b6c79f07dc728b694e98848a3
Zakariyya Mughal <zaki.mughal@gmail.com>
2017-12-08 07:04:10 -0600

    Release build of v0.003 (on master)

--- lib/Alien/ZMQ/latest.pm
+++ lib/Alien/ZMQ/latest.pm
@@ -1,6 +1,6 @@
 package Alien::ZMQ::latest;
 # ABSTRACT: Alien package for the ZeroMQ library
-$Alien::ZMQ::latest::VERSION = '0.002';
+$Alien::ZMQ::latest::VERSION = '0.003';
 use strict;
 use warnings;
 
@@ -23,7 +23,7 @@ Alien::ZMQ::latest - Alien package for the ZeroMQ library
 
 =head1 VERSION
 
-version 0.002
+version 0.003
 
 =head1 DESCRIPTION
 
